d2c3b8a6b98d5ef2ba28c44983783735c342508f
AndersonRayner <anderson_rayner@hotmail.com>
2024-04-03 12:39:26 -0700

    First pass at LED feedback based on logging status

--- arduino/software/ros_sync_node/ros_sync_node.ino
+++ arduino/software/ros_sync_node/ros_sync_node.ino
@@ -18,8 +18,7 @@
 
 #include <Adafruit_NeoPixel.h>
 
-ros::NodeHandle  nh;
-
+ros::NodeHandle nh;
 std_msgs::Time msg;
 
 // Publishers
@@ -36,6 +35,69 @@ int pin_30Hz  =  9;
 uint8_t system_health_ = sync_msgs::sensorHealth::UNKNOWN;
 uint32_t _t_last_sensors_msg_ = 0;
 
+// LED State Tracker
+class LED_StateTracker {
+    public :
+
+    enum LED_MODE
+    {
+      Solid,
+      Flashing
+    };
+    
+    void set_mode(LED_MODE mode) { led_mode_ = mode; };
+    void set_color(uint8_t red, uint8_t green, uint8_t blue) { red_ = red; green_ = green; blue_ = blue; };
+    void set_max_brightness(uint8_t max_brightness) { max_brightness_ = max_brightness; };
+    uint32_t get_color()
+    {
+        uint8_t max_brightness = max_brightness_;
+
+        if (millis() > t_next_change_)
+        {
+            t_next_change_ = millis() + 500;
+            led_flash_state_ = !led_flash_state_;
+        }
+
+        switch (led_mode_)
+        {
+          case (LED_MODE::Flashing) :
+            
+            // Change brightness around
+            if(led_flash_state_)
+            {
+                max_brightness = max_brightness_ / 10;
+            }
+            break;
+
+          case (LED_MODE::Solid) :
+          default :
+            // do nothing
+            break;
+        } 
+
+        // Limit each channel to MAX_BRIGHTNESS
+        uint8_t red = min(red_, max_brightness);
+        uint8_t green = min(green_, max_brightness);
+        uint8_t blue = min(blue_, max_brightness);
+
+        // Combine the limited channels back into the color
+        return ((uint32_t)red << 16) | ((uint32_t)green << 8) | blue;
+    };
+    
+    private :
+
+    LED_MODE led_mode_ = LED_MODE::Solid;
+
+    uint8_t red_ = 0;
+    uint8_t green_ = 0;
+    uint8_t blue_ = 0;
+
+    uint32_t max_brightness_ = 0xFF;
+    uint32_t t_next_change_ = 0;
+    bool led_flash_state_ = 0;
+
+} ledState_;
+
 void systemHealth_Callback(const sync_msgs::sensorHealthArray& msg)
 {
     system_health_ = msg.system;
@@ -43,8 +105,22 @@ void systemHealth_Callback(const sync_msgs::sensorHealthArray& msg)
     return;
 }
 
-ros::Subscriber<sync_msgs::sensorHealthArray> health_sub("/sensor_status", &systemHealth_Callback);
+void loggerStatus_Callback(const std_msgs::Bool& msg)
+{
+    if (msg.data)
+    {
+      ledState_.set_mode(LED_StateTracker::LED_MODE::Flashing);
+    }
+    else
+    {
+      ledState_.set_mode(LED_StateTracker::LED_MODE::Solid);
+    }
 
+    return;
+}
+
+ros::Subscriber<sync_msgs::sensorHealthArray> health_sub("/sensor_status", &systemHealth_Callback);
+ros::Subscriber<std_msgs::Bool> recordStart_sub("/river/record_rosbag/status", &loggerStatus_Callback);
 
 // Other
 bool core0_init = false;
@@ -65,14 +141,12 @@ void setup() {
 
   // Subscribers
   nh.subscribe(health_sub);
+  nh.subscribe(recordStart_sub);
  
   // Setup the pins
   pinMode(pin_180Hz, OUTPUT);  digitalWrite(pin_180Hz, HIGH);
   pinMode(pin_60Hz,  OUTPUT);  digitalWrite(pin_60Hz,  HIGH);
   pinMode(pin_30Hz,  OUTPUT);  digitalWrite(pin_30Hz,  HIGH);
-
-  // Init LEDs
-  init_leds();
 
   // Initialization complete
   nh.loginfo("Sync node hardware initialized");
@@ -129,14 +203,16 @@ void loop() {
 
 void setup1()
 {
-  // Wait for the first core to update everything
-  while (core0_init == 0)
-  {
+  // Wait for core0 to initialize
+  do {
     // do nothing
-    delay(500);
-  }
+    delay(1000);
+  } while (core0_init == 0);
 
-  // core0 intialized
+  // Init LEDs
+  init_leds();
+
+  // core1 intialized
 
 }
 
@@ -144,41 +220,41 @@ void loop1()
 {
 
  // Set the LED colour
-  uint32_t color = Adafruit_NeoPixel::Color(0,0,0);
 
   if (nh.connected())
   {
     if (millis() - 2UL*1000 > _t_last_sensors_msg_)
     {
       // Timeout
-      color = Adafruit_NeoPixel::Color(200, 200, 200); // White
+      ledState_.set_color(200,200,200); // White
     }
     else if (system_health_ == sync_msgs::sensorHealth::HEALTHY)
     {
       // Sensors are all running nominally
-      color = Adafruit_NeoPixel::Color(0,255, 0); // Green
+      ledState_.set_color(0,255, 0); // Green
     }
     else if (system_health_ == sync_msgs::sensorHealth::UNHEALTHY)
     {
       // ROS is running, but sensors aren't at the correct rate
-      color = Adafruit_NeoPixel::Color(255, 100, 0); // Amber
+      ledState_.set_color(255, 100, 0); // Amber
     }
     else
     {
       // Sensors are missing or state is unknown
-      color = Adafruit_NeoPixel::Color(255, 0, 0); // Red      
+      ledState_.set_color(255, 0, 0); // Red      
     } 
   }
   else
   {
     // No connection to ROS
-    color = Adafruit_NeoPixel::Color(0, 0, 255); // Blue
+    ledState_.set_color(0, 0, 255); // Blue
+    ledState_.set_mode(LED_StateTracker::LED_MODE::Solid);  // Assuming logging has stopped
   }
 
   // Update LEDs
-  update_leds(color);
+  update_leds(ledState_.get_color());
 
   // Sleep for a little bit
-  delay(200);
+  delay(20);
   
 }
